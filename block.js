db6f4fc42f38b6c44efceb182aa1df9489aa1e31
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-06-17 09:24:30 +0200

    Class Block + constructor() added

---
+++ block.js
@@ -0,0 +1,17 @@
+// Klasse Block
+class Block{
+
+    //Konstruktor + Parameter
+    constructor(timestamp, lastHash, hash, data){
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.hash = hash;
+        this.data = data;
+    }//end constructor
+
+
+
+
+
+
+}//end class Block
